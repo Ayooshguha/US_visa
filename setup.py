a0c3dd4e2af99b12e2d9fed2149fdb003c4567fd
Ayush Guha <49818716+Ayooshguha@users.noreply.github.com>
2025-02-14 17:26:59 +0530

    Update app

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Ayush Guha",
+    author_email="ayushguha@31",
+    packages=find_packages()
+)
